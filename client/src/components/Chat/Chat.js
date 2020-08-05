b8c00524106635a58c6fcb8ab707efd33364861f
Uriel Aispuro <urielalexis64@gmail.com>
2020-08-04 22:55:32 -0600

    join and disconnect emit

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,7 +1,63 @@
-import React from "react";
+import React, {useState, useEffect} from 'react';
+import queryString from 'query-string';
+import io from 'socket.io-client';
 
-const Chat = () => {
-  return <h1>Chat</h1>;
+let socket;
+
+const Chat = ({location}) => {
+	const [name, setName] = useState('');
+	const [room, setRoom] = useState('');
+	const [message, setMessage] = useState('');
+	const [messages, setMessages] = useState([]);
+	const ENDPOINT = 'localhost:5000';
+
+	useEffect(() => {
+		const {name, room} = queryString.parse(location.search);
+
+		socket = io(ENDPOINT);
+
+		setName(name);
+		setRoom(room);
+
+		socket.emit('join', {name, room}, (error) => {});
+
+		return () => {
+			socket.emit('disconnect');
+
+			socket.off();
+		};
+	}, [ENDPOINT, location.search]);
+
+	useEffect(() => {
+		socket.on('message', (message) => {
+			setMessages([...messages, message]);
+		});
+	}, [messages]);
+
+	//function for sending messages
+
+	const sendMessage = (event) => {
+		event.preventDefault();
+
+		if (message) {
+			socket.emit('sendMessage', message, () => setMessage(''));
+		}
+	};
+
+	console.log(message, messages);
+
+	return (
+		<div className='outerContainer'>
+			<div className='container'>
+				<InfoBar />
+				{/*<input
+					value={message}
+					onChange={(event) => setMessage(event.target.value)}
+					onKeyPress={(event) => (event.key === 'Enter' ? sendMessage(event) : null)}
+				/>*/}
+			</div>
+		</div>
+	);
 };
 
 export default Chat;

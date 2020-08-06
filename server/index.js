d472d0f23e2b00fb41ae67051cae63a2509261d5
Uriel Aispuro <urielalexis64@gmail.com>
2020-08-05 23:29:02 -0600

    cors, roomData

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
+const cors = require('cors');
 
 const {addUser, removeUser, getUser, getUsersInRoom} = require('./users.js');
 
@@ -25,6 +26,9 @@ io.on('connection', (socket) => {
 			.to(user.room)
 			.emit('message', {user: 'admin', text: `${user.name}, has joined!`});
 		socket.join(user.room);
+
+		io.to(user.room).emit('roomData', {room: user.room, users: getUsersInRoom(user.room)});
+
 		callback();
 	});
 
@@ -32,14 +36,20 @@ io.on('connection', (socket) => {
 		const user = getUser(socket.id);
 
 		io.to(user.room).emit('message', {user: user.name, text: message});
+		io.to(user.room).emit('roomData', {room: user.room, users: getUsersInRoom(user.room)});
 		callback();
 	});
 
 	socket.on('disconnect', () => {
-		console.log('User had left!');
+		const user = removeUser(socket.id);
+
+		if (user) {
+			io.to(user.room).emit('message', {user: 'admin', text: `${user.name} had left uwu`});
+		}
 	});
 });
 
 app.use(router);
+app.use(cors);
 
 server.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
